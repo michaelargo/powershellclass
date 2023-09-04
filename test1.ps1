39714991a7f95ebcd1dc94f45cb7a8b41964e10a
Michael Argo <mike.argo@gmail.com>
2023-09-04 14:35:59 -0500

    This reverts commit 2e637c36fca06aa452ad4b3bef4c60a20c6801e6.

--- test1.ps1
+++ test1.ps1
@@ -1,4 +1,44 @@
-[string]$hello = "hello world"
+[string]$playerInput = ""
+[bool]$proceed = $false
 
-write-host $hello
-write-host "yay"
+Clear-Host
+
+
+function getPlayerInput() {
+
+    while ($proceed -eq $false) {
+
+        $playerInput = read-host -prompt "You walk into a room with two doorwarys. One to the left and one to the right. Type 'left' or 'right' to walk through one of the doors."
+
+            if ($playerInput -eq "left") {
+                $proceed = $true
+                break
+
+               }
+               
+            elseif ($playerInput -eq "right") {
+            
+                $proceed = $true
+                break
+
+               }
+            
+            else {
+
+            write-host "You must type 'left' or 'right' to proceed."
+        }
+    }
+
+    return $playerInput
+
+}
+
+
+
+$playerInput = getPlayerInput
+
+
+write-host "You entered" $playerInput
+
+read-host "Press any key to continue..."
+
